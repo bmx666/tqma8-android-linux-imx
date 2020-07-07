330d9d0d36487b9ab88ba3843662e2e13754197f
Markus Niebel <Markus.Niebel@tq-group.com>
2020-07-07 18:35:53 +0200

    usb: chipidea: implement OTG ID pin mux handling
    usb non core registers have a special flag allow to decide between
    direct connected pin (ID direct from pad USB<n>_ID) pin and muxable
    pin (signal USB<n>_OTG_ID via IOMUX).
    
    - add needed stuff to usbmisc
    - parse in imx usb glue layer
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- drivers/usb/chipidea/ci_hdrc_imx.h
+++ drivers/usb/chipidea/ci_hdrc_imx.h
@@ -24,6 +24,7 @@ struct imx_usbmisc_data {
 	unsigned int hsic:1; /* HSIC controlller */
 	unsigned int ext_id:1; /* ID from exteranl event */
 	unsigned int ext_vbus:1; /* Vbus from exteranl event */
+	unsigned int dig_id_sel:1; /* select muxable pin for ID -> DIG_ID_SEL */
 	struct usb_phy *usb_phy;
 	enum usb_dr_mode available_role; /* runtime usb dr mode */
 	int emp_curr_control;
