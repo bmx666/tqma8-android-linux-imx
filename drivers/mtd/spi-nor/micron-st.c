2ece4fb78c65f3d9148d4ed1a492ef74dae06e88
Matthias Schiffer <matthias.schiffer@tq-group.com>
2020-12-15 13:17:35 +0100

    mtd: spi-nor: sync flags of mt25ql02g and mt25qu02g with other mt25q
    No 2G variants of the older n25q series exist, so we can assume that
    these device IDs are mt25q and support the same features as the other
    mt25q models.
    
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

--- drivers/mtd/spi-nor/micron-st.c
+++ drivers/mtd/spi-nor/micron-st.c
@@ -68,11 +68,11 @@ static const struct flash_info st_parts[] = {
 			      SECT_4K | USE_FSR | SPI_NOR_QUAD_READ |
 			      NO_CHIP_ERASE) },
 	{ "mt25ql02g",   INFO(0x20ba22, 0, 64 * 1024, 4096,
-			      SECT_4K | USE_FSR | SPI_NOR_QUAD_READ |
-			      NO_CHIP_ERASE) },
+			      SECT_4K | USE_FSR | SPI_NOR_DUAL_READ |
+			      SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
 	{ "mt25qu02g",   INFO(0x20bb22, 0, 64 * 1024, 4096,
 			      SECT_4K | USE_FSR | SPI_NOR_DUAL_READ |
-			      SPI_NOR_QUAD_READ | NO_CHIP_ERASE) },
+			      SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
 
 	{ "m25p05",  INFO(0x202010,  0,  32 * 1024,   2, 0) },
 	{ "m25p10",  INFO(0x202011,  0,  32 * 1024,   4, 0) },
