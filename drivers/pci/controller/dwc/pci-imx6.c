tq-systems/linux-tqmaxx

pci: imx: handle phy ref clk source for i.MX7
Signed-off-by: Max Merchel <Max.Merchel@tq-group.com>

drivers/pci/dwc/pci-imx6.c
@@ -1300,8 +1300,16 @@ static void imx_pcie_init_phy(struct imx_pcie *imx_pcie)
 				"failed to enable pcie regulator\n");
 
 		/* pcie phy ref clock select; 1? internal pll : external osc */
-		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR12,
-				   IMX7D_GPR12_PCIE_PHY_REFCLK_SEL, 0);
+		if (imx_pcie->ext_osc) { /* external oscillator */
+			regmap_update_bits(imx_pcie->iomuxc_gpr,
+				IOMUXC_GPR12,
+				IMX7D_GPR12_PCIE_PHY_REFCLK_SEL, 0);
+		} else { /* internel oscillator */
+			regmap_update_bits(imx_pcie->iomuxc_gpr,
+				IOMUXC_GPR12,
+				IMX7D_GPR12_PCIE_PHY_REFCLK_SEL,
+				IMX7D_GPR12_PCIE_PHY_REFCLK_SEL);
+		}
 	} else if (imx_pcie->variant == IMX6SX) {
 		/* Force PCIe PHY reset */
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR5,

bmx666/tqma8-android-linux-imx

drivers/pci/controller/dwc/pci-imx6.c
@@ -1647,8 +1647,18 @@ static void imx6_pcie_init_phy(struct imx6_pcie *imx6_pcie)
 		break;
 	case IMX7D:
 	case IMX7D_EP:
-		regmap_update_bits(imx6_pcie->iomuxc_gpr, IOMUXC_GPR12,
-				   IMX7D_GPR12_PCIE_PHY_REFCLK_SEL, 0);
+		if (imx6_pcie->ext_osc) {
+			/* external oscillator */
+			regmap_update_bits(imx6_pcie->iomuxc_gpr,
+				IOMUXC_GPR12,
+				IMX7D_GPR12_PCIE_PHY_REFCLK_SEL, 0);
+		} else {
+			/* internal oscillator */
+			regmap_update_bits(imx6_pcie->iomuxc_gpr,
+				IOMUXC_GPR12,
+				IMX7D_GPR12_PCIE_PHY_REFCLK_SEL,
+				IMX7D_GPR12_PCIE_PHY_REFCLK_SEL);
+		}
 		break;
 	case IMX6SX:
 	case IMX6SX_EP: