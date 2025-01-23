56b27b4db1d7ebfd7396a781e35eb877bc16af4b
Maxim Paymushkin <bmx666@users.noreply.github.com>
2025-01-23 01:53:03 +0100

    drm: bridge: sn65dsi83: fix compile

--- drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_brg.c
+++ drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_brg.c
@@ -13,7 +13,10 @@
 #include <linux/of_graph.h>
 #include <linux/slab.h>
 
-#include <drm/drmP.h>
+#include <drm/drm_vblank.h>
+#include <drm/drm_bridge.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_print.h>
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_edid.h>
--- drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_drv.c
+++ drivers/gpu/drm/bridge/sn65dsi83/sn65dsi83_drv.c
@@ -13,7 +13,10 @@
 #include <linux/slab.h>
 #include <linux/regulator/consumer.h>
 
-#include <drm/drmP.h>
+#include <drm/drm_vblank.h>
+#include <drm/drm_bridge.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_print.h>
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_edid.h>
@@ -64,7 +67,7 @@ static int sn65dsi83_connector_get_modes(struct drm_connector *connector)
 	dev_dbg(dev, "%s\n", __func__);
 
 	if (sn65dsi83->panel) {
-		ret = drm_panel_get_modes(sn65dsi83->panel);
+		ret = drm_panel_get_modes(sn65dsi83->panel, connector);
 		return ret;
 	}
 
@@ -90,9 +93,9 @@ static int sn65dsi83_connector_get_modes(struct drm_connector *connector)
 	if (brg->vm.flags & DISPLAY_FLAGS_DE_LOW)
 		*bus_flags |= DRM_BUS_FLAG_DE_LOW;
 	if (brg->vm.flags & DISPLAY_FLAGS_PIXDATA_NEGEDGE)
-		*bus_flags |= DRM_BUS_FLAG_PIXDATA_NEGEDGE;
+		*bus_flags |= DRM_BUS_FLAG_PIXDATA_DRIVE_NEGEDGE;
 	if (brg->vm.flags & DISPLAY_FLAGS_PIXDATA_POSEDGE)
-		*bus_flags |= DRM_BUS_FLAG_PIXDATA_POSEDGE;
+		*bus_flags |= DRM_BUS_FLAG_PIXDATA_DRIVE_POSEDGE;
 
 	ret = drm_display_info_set_bus_formats(&connector->display_info,
 					       &bus_format, 1);
@@ -250,10 +253,6 @@ static int sn65dsi83_bridge_attach(struct drm_bridge *bridge)
 
 	if (ret)
 		return ret;
-
-	/* attach panel to bridge */
-	if (sn65dsi83->panel)
-		ret = drm_panel_attach(sn65dsi83->panel, &sn65dsi83->connector);
 
 	return ret;
 }
